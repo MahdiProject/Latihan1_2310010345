67ba677e7910b266c742f1c3fa9f0a6f3974a339
MahdiProject <muhammdmahdi.2018@gmail.com>
2025-06-11 15:48:50 +0800

    membuat mutator

--- src/latihan3/Rumus.java
+++ src/latihan3/Rumus.java
@@ -3,4 +3,12 @@ package latihan3;
 public class Rumus {
     //Muhammd Mahdi, 2310010345, 4D
     private double sisi,panjang,lebar,hasil;
+    
+    public double setLuasPersegi(){
+        return sisi * sisi;
+    }
+    
+    public double setLuasPersegiPanjang(){
+        return panjang * lebar;
+    }
 }
