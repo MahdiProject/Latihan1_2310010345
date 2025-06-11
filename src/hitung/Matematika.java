5598b784261d9363d39d7f7f3d50c3f3b38e075c
MahdiProject <muhammdmahdi.2018@gmail.com>
2025-06-11 12:52:48 +0800

    Melengkapi method matematika

--- src/hitung/Matematika.java
+++ src/hitung/Matematika.java
@@ -11,7 +11,23 @@ public class Matematika {
     }
     
     //membuat method setPenjumlahan
-    double setPenjumlahan(){
+    public double setPenjumlahan(){
         return bil1 + bil2;
+    }
+    
+    
+    //membuat method setPengurangan
+    public double setPengurangan(){
+        return bil1 - bil2;
+    }
+    
+    //membuat method setPerkalian
+    public double setPerkalian(){
+        return bil1 * bil2;
+    }
+    
+    //membuat method setPembagian
+    public double setPembagian(){
+        return bil1 / bil2;
     }
 }
