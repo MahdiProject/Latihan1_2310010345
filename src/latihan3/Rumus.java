93d21aa01f8cf602abb79db6d58fd781e041d46f
MahdiProject <muhammdmahdi.2018@gmail.com>
2025-06-11 15:58:33 +0800

    membuat accessor dan constructor

--- src/latihan3/Rumus.java
+++ src/latihan3/Rumus.java
@@ -4,6 +4,18 @@ public class Rumus {
     //Muhammd Mahdi, 2310010345, 4D
     private double sisi,panjang,lebar,hasil;
     
+    public double getHasil(){
+        return hasil;
+    }
+    
+    //membuat construktor
+    public Rumus(double sisi, double panjang, double lebar, double hasil){
+        this.sisi = sisi;
+        this.panjang = panjang;
+        this.lebar = lebar;
+        this.hasil = hasil;
+    }
+    
     public double setLuasPersegi(){
         return sisi * sisi;
     }
