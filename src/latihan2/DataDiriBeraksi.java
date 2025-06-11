ed9f97604a90dfcc7fc80711cb19bacf7f55889c
MahdiProject <muhammdmahdi.2018@gmail.com>
2025-06-11 15:35:26 +0800

    Menyiapkan latihan2

---
+++ src/latihan2/DataDiriBeraksi.java
@@ -0,0 +1,7 @@
+package latihan2;
+
+public class DataDiriBeraksi {
+    public static void main(String[] args){
+        DataDiri mahdi = new DataDiri("Muhammad Mahdi", "2310010345", "4D");
+    }
+}
