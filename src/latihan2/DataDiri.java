6213bfd52b94033b52abf5c87e9f354b2ef4ea4e
MahdiProject <muhammdmahdi.2018@gmail.com>
2025-06-11 15:25:37 +0800

    initial commit

---
+++ src/latihan2/DataDiri.java
@@ -0,0 +1,11 @@
+package latihan2;
+
+public class DataDiri {
+    String nama, npm, kelas;
+    
+    public DataDiri(String nama, String npm, String kelas){
+        this.nama = nama;
+        this.npm = npm;
+        this.kelas = kelas;
+    }
+}
