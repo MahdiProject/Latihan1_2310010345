285d8f84b785dd98be5ac99053f39f1bb78752e4
MahdiProject <muhammdmahdi.2018@gmail.com>
2025-06-11 13:05:07 +0800

    Membuat class MatematikaBeraksi

--- src/hitung/MatematikaBeraksi.java
+++ src/hitung/MatematikaBeraksi.java
@@ -1,14 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package hitung;
-
-/**
- *
- * @author HP
- */
+ 
 public class MatematikaBeraksi {
-    
+    public static void main(String[] args){
+        //membuat objek
+        Matematika mahdi = new Matematika(4,5);
+        System.out.println("Hasil penjumlahan: "+mahdi.setPenjumlahan());
+        System.out.println("Hasil pengurangan: "+mahdi.setPengurangan());
+        System.out.println("Hasil perkalian: "+mahdi.setPerkalian());
+        System.out.println("Hasil pembagian: "+mahdi.setPembagian());
+    }
 }
